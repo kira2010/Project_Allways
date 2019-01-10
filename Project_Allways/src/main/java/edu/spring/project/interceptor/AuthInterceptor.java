7053a4e367ef19c2b88b72a84e6ec9bb87c52f40
yunheehyeon <ok94life@daum.com>
2019-01-10 14:04:20 +0900

    intercepter 준비

---
+++ Project_Allways/src/main/java/edu/spring/project/interceptor/AuthInterceptor.java
@@ -0,0 +1,18 @@
+package edu.spring.project.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AuthInterceptor extends HandlerInterceptorAdapter {
+	
+	// 로그인 되어있지 않으면 -> 로그인 페이지로 이동
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return super.preHandle(request, response, handler);
+	}
+	
+}
