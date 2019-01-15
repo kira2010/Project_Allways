4b27b9d78c9a4a222030c68a8695a2586c744a73
kira2010 <kira20105@gmail.com>
2019-01-15 17:32:24 +0900

    logger 사용 및 System.out.println 사용 코드 -> logger 로 변경

--- Project_Allways/src/main/java/edu/spring/project/controller/UsersController.java
+++ Project_Allways/src/main/java/edu/spring/project/controller/UsersController.java
@@ -6,6 +6,8 @@ import java.util.Date;
 
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,25 +16,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import edu.spring.project.domain.User;
 import edu.spring.project.service.UserService;
 
-
-
 @Controller
 @RequestMapping("/users")
 public class UsersController {
-
+	private final Logger logger = LoggerFactory.getLogger(UsersController.class);
+	
 	@Autowired
 	private UserService userService;
 	
 	// 회원가입
 	@RequestMapping(value="/signUp", method=RequestMethod.GET)
 	public String signUp() {
+		logger.info("signUp() GET 호출");
+		
 		return "users/signUp";
 	}
 	
 	@RequestMapping(value="/signUp", method=RequestMethod.POST)
 	public String signUp(User user, String birthY, String birthM, String birthD) throws Exception {
-		System.out.println("signUp() post: user= " + user);
-		System.out.println(birthY + "-" + birthM + "-" + birthD);
+		logger.info("signUp({}) POST 호출", user);
+		logger.info("생년월일 결과 : {}-{}-{}", birthY, birthM, birthD);
+		
 		LocalDate ld = LocalDate.of(Integer.parseInt(birthY), 
 				Integer.parseInt(birthM),
 				Integer.parseInt(birthD));
@@ -41,23 +45,26 @@ public class UsersController {
 		
 		userService.insert(user);
 		
-		
 		return "users/welcome";
 	}
 	
 	// 로그인
 	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public String login() {
+		logger.info("login() GET 호출");
+		
 		return "login";
 	}
 
 	@RequestMapping(value="/login", method=RequestMethod.POST)
 	public String login(User user, HttpSession session) throws Exception {
+		logger.info("login() POST 호출");
+		
 		User checkUser = userService.login(user);
 		User loginUser = new User();
 		loginUser.setUno(checkUser.getUno());
 		loginUser.setUserId(checkUser.getUserId());
-		if (loginUser != null) {
+		if (checkUser != null) {
 			session.setAttribute("check", loginUser);
 			return "redirect:/";
 		} else {
@@ -66,9 +73,10 @@ public class UsersController {
 	}
 
 	// 아이디 찾기 폼
-	@RequestMapping(value="/find_id_form")
+	@RequestMapping(value="/findId")
 	public  String findId() throws Exception{
-		return "users/find_id_form";
+		logger.info("findId() 호출");
+		
+		return "users/findId";
 	}
-	
 }
