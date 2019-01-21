0a31a4bebf533ac4d9126724147d75677eefb2bb
kira2010 <kira20105@gmail.com>
2019-01-21 10:53:34 +0900

    searchKeyword 재수정

--- Project_Allways/src/test/java/edu/spring/project/DaoTest.java
+++ Project_Allways/src/test/java/edu/spring/project/DaoTest.java
@@ -68,7 +68,7 @@ public class DaoTest {
 		
 		//List<Board> myTimeLineList = boardDao.readMyTimeLine(1, 0);
 		
-		List<Board> searchList = boardDao.searchByKeyword(1, "공개", 0);
+		List<Board> searchList = boardDao.searchByKeyword(1, "test", 0);
 		
 		if(searchList != null && searchList.size() > 0) {
 			for(Board b : searchList) {
